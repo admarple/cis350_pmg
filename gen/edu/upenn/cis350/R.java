d34e0d8e2a5ccc0d49a57d95dbccf28df6b94efc
Alex Marple <admarple@gmail.com>
2012-02-23 00:41:11 -0500

    Added some strings/images to resources

--- gen/edu/upenn/cis350/R.java
+++ gen/edu/upenn/cis350/R.java
@@ -8,16 +8,50 @@
 package edu.upenn.cis350;
 
 public final class R {
+    public static final class array {
+        public static final int poi_info=0x7f050001;
+        public static final int poi_title=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int debbie_cohen_gardens5=0x7f020000;
+        public static final int garden_facade__gk=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int standard_pmg_logo_jpeg=0x7f020003;
+        public static final int standard_pmg_logo_png=0x7f020004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
+        public static final int accessibility_header=0x7f04001a;
+        public static final int accessibility_info=0x7f04001b;
+        public static final int admission_ga_header=0x7f040010;
+        public static final int admission_ga_info=0x7f040012;
+        public static final int admission_ga_rates=0x7f040011;
+        public static final int admission_header=0x7f04000f;
+        public static final int admission_tours_header=0x7f040013;
+        public static final int admission_tours_info=0x7f040015;
+        public static final int admission_tours_rates=0x7f040014;
         public static final int app_name=0x7f040001;
+        public static final int contact_address=0x7f040003;
+        public static final int contact_email=0x7f040005;
+        public static final int contact_header=0x7f040002;
+        public static final int contact_phone=0x7f040004;
+        public static final int contact_website=0x7f040006;
         public static final int hello=0x7f040000;
+        public static final int hours_header=0x7f040007;
+        public static final int hours_holiday=0x7f04000d;
+        public static final int hours_holiday_header=0x7f04000c;
+        public static final int hours_note=0x7f04000e;
+        public static final int hours_summer=0x7f040009;
+        public static final int hours_summer_header=0x7f040008;
+        public static final int hours_winter=0x7f04000b;
+        public static final int hours_winter_header=0x7f04000a;
+        public static final int parking_header=0x7f040018;
+        public static final int parking_info=0x7f040019;
+        public static final int policy_header=0x7f040016;
+        public static final int policy_info=0x7f040017;
     }
 }
